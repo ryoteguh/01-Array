18ef7625ed5ecbf7b515c756df4efeadaa835119
ryoteguh <theryomi@gmail.com>
2026-03-12 15:16:56 +0800

    memisahkan input, proses, dan output

--- Soal_1.py
+++ Soal_1.py
@@ -1,14 +1,21 @@
-arr = []
-for i in range(10):
-    angka = int(input('Masukkan nilai: '))
-    arr.append(angka)
-print('Array            : ',arr)
-print('======================================')
-print('Nilai tertinggi  :', max(arr))
-print("Nilai Terendah   :", min(arr))
-print("Rata-Rata        :",sum(arr)/len(arr))
-#hitung jumlah mahasiswa lulus
-lulus = [n for n in arr if n>=60]
-jumlah_lulus = len(lulus)
-print("Jumlah Mahasiswa yang lulus: ", jumlah_lulus)
+def sistem_nilai_mahasiswa():
+    #definisi array kosong untuk input nilai
+    nilai = []
+    for i in range(10):
+        angka = int(input(f'Masukkan nilai ke-{i+1}: '))
+        nilai.append(angka)
+    #proses nilai
+    tertinggi = max(nilai)
+    terendah = min(nilai)
+    rata_rata = sum(nilai)/len(nilai)
+    lulus = [n for n in nilai if n >=60]
+    jumlah_lulus = len(lulus)
+    #menampilkan informasi dari hasil proses
+    print('Nilai dalam Array            : ',nilai)
+    print('======================================')
+    print('Nilai tertinggi  :', tertinggi)
+    print("Nilai Terendah   :", terendah)
+    print("Rata-Rata        :", rata_rata)
+    print("Jumlah Mahasiswa yang lulus: ", jumlah_lulus)
+sistem_nilai_mahasiswa()
 
